1e5580ab5c83db8a9bb5ebbb426e959c6e92c8ea
TomSpencer-Dev <Thomas.spencer2034@gmail.com>
2026-03-10 12:49:29 -0400

    Setup Settings Fragment to inflate fragment_settings.xml

--- app/src/main/java/com/example/expenseeye/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/expenseeye/ui/settings/SettingsFragment.java
@@ -1,14 +1,39 @@
 package com.example.expenseeye.ui.settings;
 
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.expenseeye.R;
+import com.example.expenseeye.repository.settings.SettingsRepository;
+import com.example.expenseeye.repository.backup.BackupRepository;
+import com.example.expenseeye.service.backup.JSONBackupService;
 import com.example.expenseeye.viewmodel.settings.SettingsViewModel;
 
-public class SettingsFragment {
+public class SettingsFragment extends Fragment {
 
-    private final SettingsViewModel settingsViewModel;
-    public SettingsFragment(SettingsViewModel settingsViewModel){
-        this.settingsViewModel = settingsViewModel;
+    private SettingsViewModel settingsViewModel;
+
+    public SettingsFragment() {
+
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        settingsViewModel = new SettingsViewModel(
+                new SettingsRepository(requireContext()),
+                new BackupRepository("backup.json")
+        );
+    }
 
     public void saveSettings(String key, String value) {
         settingsViewModel.changeString(key, value);
@@ -31,10 +56,9 @@ public class SettingsFragment {
 
     public void showBackupResult(Boolean success) {
         if (success) {
-            System.out.println("Backup operation successsful");
+            System.out.println("Backup operation successful");
         } else {
             System.out.println("Backup operation failed");
         }
     }
-
 }
